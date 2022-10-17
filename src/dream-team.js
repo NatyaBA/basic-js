4d0438c95e34b33f43e2ee049c053363d3b3142b
Anastasia <Anastasia32_01@mail.ru>
2022-10-17 16:43:01 +0300

    docs:add five task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (typeof members !== 'string' && !Array.isArray(members)) { return false }
+    let newArr = [];
+    members.forEach(item => {
+        newArr.push(item.substr(0,1));
+    })
+    return(newArr.sort());
 }
 
 module.exports = {
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -6,24 +6,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 const chainMaker = {
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.chain.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    let arr = [];
+    for (let i = 1; i <= this.chain.length-1; i++) { 
+      arr[i] = this.chain[i]; 
+    }
+    if (arr.indexOf(position) === -1) {
+      this.chain = [];
+      throw new Error("You can't remove incorrect link!")
+    } else {
+      this.chain.splice(position - 1, 1);
+      return this;
+    }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let finish = this.chain.join('~~');
+    this.chain = [];
+    return finish;
   }
 };
 
